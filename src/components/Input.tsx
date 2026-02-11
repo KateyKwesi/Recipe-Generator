334287ec197bb587515e64ec1044519e929fd17e
kateykwesi <kateykwesi001@gmail.com>
2026-02-11 01:20:10 +0000

    use form + FormData to get input value

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,26 @@
 function Input() {
+  const handleSubmit = (formdata: FormData) => {
+    const value = formdata.get(`ingredient`);
+    console.log(value);
+  };
+
   return (
     <div className="mt-15 gap-2 flex items-center justify-center">
-      <input
-        type="text"
-        className="w-99.5 h-9.5 px-3 py-4 tracking-normal  border border-[#D1D5DB] rounded placeholder:text-sm placeholder:pl-2"
-        placeholder="e.g beans"
-        name="ingredient"
-      />
-      <button className="bg-black text-white leading-5 font-sans h-9.5 rounded hover:scale-105 hover:cursor-pointer">
-        <p className="px-3 text-sm"> + Add Ingredient</p>
-      </button>
+      <form action={handleSubmit}>
+        <input
+          type="text"
+          aria-label="add ingredient"
+          className="w-99.5 h-9.5 px-3 py-4 tracking-normal  border border-[#D1D5DB] rounded placeholder:text-sm placeholder:pl-2"
+          placeholder="e.g beans"
+          name="ingredient"
+        />
+        <button
+          type="submit"
+          className="bg-black px-3 text-sm text-white leading-5 font-sans h-9.5 rounded hover:scale-105 hover:cursor-pointer"
+        >
+          Add Ingredient
+        </button>
+      </form>
     </div>
   );
 }
